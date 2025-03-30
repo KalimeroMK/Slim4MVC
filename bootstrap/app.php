7ee74707872a566632dd900b6b5101ef50babc1b
KalimeroMK <zbogoevski@gmail.com>
2025-03-30 12:15:50 +0200

    add session support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,18 +4,12 @@ declare(strict_types=1);
 
 use DI\Container;
 use Slim\Factory\AppFactory;
-use Support\Blade;
+use Slim\Middleware\Session;
 
 require __DIR__.'/../vendor/autoload.php';
 
 $container = new Container;
 
-$container->set(Blade::class, function (): Blade {
-    $views = __DIR__.'/../resources/views';
-    $cache = __DIR__.'/../storage/cache/';
-
-    return new Blade($views, $cache);
-});
 $settings = require __DIR__.'/../bootstrap/settings.php';
 $settings($container);
 
@@ -23,6 +17,13 @@ AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
+$app->add(
+    new Session([
+        'name' => 'dummy_session',
+        'autorefresh' => true,
+        'lifetime' => '1 hour',
+    ])
+);
 (require __DIR__.'/../bootstrap/database.php');
 (require __DIR__.'/../routes/web.php')($app);
 (require __DIR__.'/../routes/api.php')($app);
