7f12dd26b9098a1dc09ed81026fc69a2abfb6d63
KalimeroMK <zbogoevski@gmail.com>
2025-03-31 16:38:56 +0200

    remove not used settings file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,8 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use DI\Container;
 use Illuminate\Database\Capsule\Manager as Capsule;
@@ -10,18 +10,12 @@ use Slim\Factory\AppFactory;
 
 $container = new Container();
 
-// Load the settings (if it's a callable)
-$settings = require __DIR__.'/../bootstrap/settings.php';
-if (is_callable($settings)) {
-    $settings($container);
-}
-
 // Create an instance of Capsule and load the database configuration
 $capsule = new Capsule;
-require __DIR__.'/../bootstrap/database.php';
+require __DIR__ . '/../bootstrap/database.php';
 
 // Load the validation configuration
-$validation = require __DIR__.'/../bootstrap/validation.php';
+$validation = require __DIR__ . '/../bootstrap/validation.php';
 $validation($container, $capsule);
 
 // Set the container in AppFactory
@@ -31,7 +25,7 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 // Add routes and other services
-(require __DIR__.'/../routes/web.php')($app);
-(require __DIR__.'/../routes/api.php')($app);
+(require __DIR__ . '/../routes/web.php')($app);
+(require __DIR__ . '/../routes/api.php')($app);
 
 $app->run();
