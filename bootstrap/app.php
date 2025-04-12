fe8b5e3e4f1435ad0763f3328776a88db3a7307e
KalimeroMK <zbogoevski@gmail.com>
2025-04-12 12:56:34 +0200

    move container build to specific file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,41 +6,29 @@ declare(strict_types=1);
 
 require __DIR__.'/../vendor/autoload.php';
 
-use DI\ContainerBuilder;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Slim\Factory\AppFactory;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 
-// Initialize PHP-DI container
-$containerBuilder = new ContainerBuilder();
-$containerBuilder->useAutowiring(true);
-$containerBuilder->addDefinitions(require __DIR__.'/../bootstrap/dependencies.php');
-$container = $containerBuilder->build();
-
-// Start session
-$storage = new NativeSessionStorage();
-$session = new Session($storage);
-$session->start();
-$container->set(Session::class, fn (): Session => $session);
+// Initialize container
+$container = require __DIR__ . '/container.php';
 
 // Configure database
 $capsule = new Capsule;
-require __DIR__.'/../bootstrap/database.php';
+require __DIR__.'/database.php';
 
 // Configure validation
-$validation = require __DIR__.'/../bootstrap/validation.php';
+$validation = require __DIR__.'/validation.php';
 $validation($container, $capsule);
 
 // Configure Blade templating
-(require __DIR__.'/../bootstrap/blade.php')($container);
+(require __DIR__.'/blade.php')($container);
 
 // Set the container in Slim
 AppFactory::setContainer($container);
 $app = AppFactory::createFromContainer($container);
 
 // Load middleware
-(require __DIR__.'/../bootstrap/middleware.php')($app, $container);
+(require __DIR__.'/middleware.php')($app, $container);
 
 // Load routes
 (require __DIR__.'/../routes/web.php')($app);
