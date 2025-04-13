1af876511587f9c483ef19a5f026f09b3ee47680
KalimeroMK <zbogoevski@gmail.com>
2025-04-13 11:55:54 +0200

    fix redirect error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
 
 abstract class Controller
 {
@@ -17,5 +18,11 @@ abstract class Controller
     protected function getContainer(): ContainerInterface
     {
         return $this->container;
+    }
+
+    protected function redirect(string $url): ResponseInterface
+    {
+        $response = $this->container->get(ResponseInterface::class);
+        return $response->withHeader('Location', $url)->withStatus(302);
     }
 }
